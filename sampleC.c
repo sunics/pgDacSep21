676977e16d0c6086bad19f55855e223f480dddb7
Sunitha <sunics@gmail.com>
2021-10-06 14:59:58 +0530

    second commit without main function change

--- sampleC.c
+++ sampleC.c
@@ -4,7 +4,7 @@ void addTwo()
 {	int n1,n2;
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
-	printf("Sum of the two numbers is %d", n1+n2);
+	printf("Sum of %d and %d  is %d", n1,n2,n1+n2);
 	
 }
 
