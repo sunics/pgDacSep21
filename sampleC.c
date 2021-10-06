f5ab709d749a98ee9046e0c490fec4714b16033a
Sunitha <sunics@gmail.com>
2021-10-06 14:57:22 +0530

    first copy of my file

---
+++ sampleC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void addTwo()
+{	int n1,n2;
+	printf("Enter Two integers ");
+	scanf("%d,%d",&n1,&n2);
+	printf("Sum of the two numbers is %d", n1+n2);
+	
+}
+
+
+void main()
+{
+	
+}
