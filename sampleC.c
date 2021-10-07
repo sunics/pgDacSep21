0db47d8ecca1c0d98abfd609f393031053c4c320
SIDHARTH K S <91144938+SIDHARTHKS60@users.noreply.github.com>
2021-10-07 11:49:55 +0530

    Changed the braces

--- sampleC.c
+++ sampleC.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 
 void addTwo()
-{	int n1,n2;
+     {	
+	int n1,n2;
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
 	printf("Sum of %d and %d  is %d", n1,n2,n1+n2);
 	
-}
+     }
 
 
 void main()
-{
+    {
 	addTwo();
-}
+    }
