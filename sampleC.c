b7578f486d9428c770d65e8db23b444b1a9e9314
abhishekpatel18 <91961959+abhishekpatel18@users.noreply.github.com>
2021-10-07 12:13:57 +0530

    Update sampleC.c

--- sampleC.c
+++ sampleC.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
-float addTwo()
+int addTwo()
 {	int n1,n2;
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
-	return (n1+n2)/2;
+	return (n1+n2);
 	
 }
 
 
 void main()
 {
-	float a;
+	int a;
 	a=addTwo();
-	printf("Sum of two number is %f ",a);
+	printf("Sum of two number is %d ",a);
 	
 }
