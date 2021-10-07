116dddb3731bfcf0b3c399be2c99728aeaad5f70
rahulk1997kubasad <50587195+rahulk1997kubasad@users.noreply.github.com>
2021-10-07 11:54:22 +0530

    this is from Rahul

--- sampleC.c
+++ sampleC.c
@@ -5,7 +5,7 @@ void addTwo()
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
 	printf("Sum of %d and %d  is %d", n1,n2,n1+n2);
-	
+	printf("please accept or reject but dont leave it");
 }
 
 
