40e7544e9ea29e183e8e85764575b329f585118a
sanketdesai1235 <91977847+sanketdesai1235@users.noreply.github.com>
2021-10-07 12:08:17 +0530

    chnages done

--- sampleC.c
+++ sampleC.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 
-void addTwo()
+void main
 {	int n1,n2;
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
 	printf("Sum of %d and %d  is %d", n1,n2,n1+n2);
 	
-}
-
-
-void main()
-{
-	addTwo();
 }
