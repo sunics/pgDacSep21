dd523c8cf0bf12b06030da6c05dade357198b6f4
sunics <sunics@gmail.com>
2021-10-07 12:13:49 +0530

    Accepted subtract function by Govind

--- sampleC.c
+++ sampleC.c
@@ -4,7 +4,7 @@ void subTwo()
 {	int n1,n2;
 	printf("Enter Two integers ");
 	scanf("%d,%d",&n1,&n2);
-	printf("Sum of %d and %d  is %d", n1,n2,n1-n2);
+	printf("Difference of %d and %d  is %d", n1,n2,n1-n2);
 	
 }
 void main()
