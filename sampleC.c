f4efc7c941b66adef327d66f8ae389e6f618ffdd
akash8375 <64433662+akash8375@users.noreply.github.com>
2021-10-06 23:54:11 -0700

    Update sampleC.c

--- sampleC.c
+++ sampleC.c
@@ -1,15 +1,1 @@
-#include<stdio.h>
 
-void divide()
-{	int n1,n2;
-	printf("Enter Two integers ");
-	scanf("%d,%d",&n1,&n2);
-	printf("Sum of %d and %d  is %f", n1,n2,n1\n2);
-	
-}
-
-
-void main()
-{
-	divide();
-}
